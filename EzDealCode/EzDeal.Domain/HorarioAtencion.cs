1bec8a14f6ad79420a8580fd7b896ab06823ecae
Cesar <u201710947@upc.edu.pe>
2019-06-06 15:34:02 -0500

    horario

--- EzDealCode/EzDeal.Domain/HorarioAtencion.cs
+++ EzDealCode/EzDeal.Domain/HorarioAtencion.cs
@@ -2,6 +2,9 @@ namespace EzDeal.Domain
 {
     public class HorarioAtencion
     {
-        
+        public string dia {get; set;}
+        public Anuncio anuncio {get; set;}
+        public string hora_inicio {get; set;}
+        public string hora_fin {get; set;}  
     }
 }
