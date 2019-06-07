a2e6b90fb838a5cfe33f41693c58ca698d44809f
Bryan <bbqms@hotmail.com>
2019-06-06 22:15:13 -0500

    ya se conectaaaaa

--- EzDealCode/EzDeal.Api/Startup.cs
+++ EzDealCode/EzDeal.Api/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;   
+using EzDeal.Repository.Context; 
 
 namespace EzDeal.Api
 {
@@ -25,6 +27,7 @@ namespace EzDeal.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
--- EzDealCode/EzDeal.Repository/Context/ApplicationDbContext.cs
+++ EzDealCode/EzDeal.Repository/Context/ApplicationDbContext.cs
@@ -10,7 +10,7 @@ namespace EzDeal.Repository.Context
         public DbSet<Servicio> ServicioDb {get; set;}
         public DbSet<Solicitud> SolicitudDb{get; set;}
         public DbSet<Reseña> ReseñaDb {get; set;}
-        public DbSet<HorarioAtencion> HorarioAtencionDb {get; set;}
+        //public DbSet<HorarioAtencion> HorarioAtencionDb {get; set;}
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){
             
